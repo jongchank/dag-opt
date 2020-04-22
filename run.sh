0f884aa5ebbc7aabd3f463fa31dfd2a44f0b0521
jongchank <jongchank@gmail.com>
2020-04-21 23:18:02 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # (a)
 ./opt our a 4 100 60 90 40 
-./opt our a 4 50 70 90 30
+./opt our a 4 50 70 100 90
 ./opt our a 4 80 30 50 100
 ./opt our a 4 40 90 120 50
 ./opt our a 4 80 130 70 60 
 ./opt exh a 4 100 60 90 40 
-./opt exh a 4 50 70 90 30
+./opt exh a 4 50 70 100 90
 ./opt exh a 4 80 30 50 100
 ./opt exh a 4 40 90 120 50
 ./opt exh a 4 80 130 70 60 
@@ -38,52 +38,8 @@
 ./opt our d 6 50 30 90 20 70 60
 ./opt our d 6 30 20 110 70 60 50
 ./opt our d 6 70 80 50 100 40 90
-./opt exh d 6 80 60 70 40 110 80
-./opt exh d 6 50 70 40 70 30 110
-./opt exh d 6 50 30 90 20 70 60
-./opt exh d 6 30 20 110 70 60 50
-./opt exh d 6 70 80 50 100 40 90
-# (e)
-./opt our e 4 130 110 90 80
-./opt our e 4 70 130 60 100
-./opt our e 4 40 80 140 90
-./opt our e 4 60 70 90 150
-./opt our e 4 110 90 140 30 
-./opt exh e 4 130 110 90 80
-./opt exh e 4 70 130 60 100
-./opt exh e 4 40 80 140 90
-./opt exh e 4 60 70 90 150
-./opt exh e 4 110 90 140 30 
-# (f)
-./opt our f 5 140 60 150 120 90
-./opt our f 5 130 150 80 50 40
-./opt our f 5 70 140 40 90 30
-./opt our f 5 120 80 110 60 100
-./opt our f 5 100 90 150 90 120
-./opt exh f 5 140 60 150 120 90
-./opt exh f 5 130 150 80 50 40
-./opt exh f 5 70 140 40 90 30
-./opt exh f 5 120 80 110 60 100
-./opt exh f 5 100 90 150 90 120
-# (g)
-./opt our g 5 90 100 80 90 30
-./opt our g 5 110 30 130 140 110
-./opt our g 5 80 140 150 90 120
-./opt our g 5 100 150 50 30 60
-./opt our g 5 130 50 40 80 130  
-./opt exh g 5 90 100 80 90 30
-./opt exh g 5 110 30 130 140 110
-./opt exh g 5 80 140 150 90 120
-./opt exh g 5 100 150 50 30 60
-./opt exh g 5 130 50 40 80 130  
-# (h)
-./opt our h 6 70 30 30 110 40 70
-./opt our h 6 40 30 90 140 100 40
-./opt our h 6 30 120 30 70 60 50
-./opt our h 6 40 30 110 80 20 110
-./opt our h 6 50 100 40 50 30 70 
-./opt exh h 6 70 30 30 110 40 70
-./opt exh h 6 40 30 90 140 100 40
-./opt exh h 6 30 120 30 70 60 50
-./opt exh h 6 40 30 110 80 20 110
-./opt exh h 6 50 100 40 50 30 70 
+#./opt exh d 6 80 60 70 40 110 80
+#./opt exh d 6 50 70 40 70 30 110
+#./opt exh d 6 50 30 90 20 70 60
+#./opt exh d 6 30 20 110 70 60 50
+#./opt exh d 6 70 80 50 100 40 90
