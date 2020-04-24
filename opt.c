6f1f42ac6e0433d59596c6de7bffa8ef96eb5583
jongchank <jongchank@gmail.com>
2020-04-23 20:23:33 -0700

    Add command line options and regression testing

--- opt.c
+++ opt.c
@@ -1,32 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
 #include <math.h>
 #include <string.h>
 #include <sys/time.h>
 
-#define N_MAX    10
-#define UB       1.0
-#define ALPHA    0.01
-#define BETA     0.01
-#define P_STRIDE 10.0
-#define P_MIN    P_STRIDE
-#define P_MAX    1000.0
-
-#define SHOW_TIMING   0
-
+/* support macros for finding maximum values */
 #define MAX2(a, b)          ((a) > (b) ? (a) : (b))
 #define MAX3(a, b, c)       (MAX2(MAX2((a), (b)), (c)))
 #define MAX4(a, b, c, d)    (MAX2(MAX2(MAX2((a), (b)), (c)), (d)))
 #define MAX5(a, b, c, d, e) (MAX2(MAX2(MAX2(MAX2((a), (b)), (c)), (d)), (e)))
 
-static int run_ours(double e[], char t, int n);
-static int run_exhaustive(double e[], char t, int n);
-static int get_n(char t);
-static double east(double e[], char t, int n);
-static double jconv(double p[], double e[], char t, int n);
+/* max number of runnables */
+#define N_MAX    10
+
+/* default options */
+#define TIMING   0      /* whether to show timing information */
+#define UB       1.0    /* utilization bound */
+#define ALPHA    0.01   /* alpha constant for control cost function */
+#define BETA     0.01   /* beta constant for control cost function */
+#define P_MIN    10.0   /* minimum period for exhaustive search */
+#define P_MAX    1000.0 /* maximum period for exhaustive search */
+#define P_STEP   10.0   /* step for exhaustive search */
+
+struct options {
+    int timing;
+    double ub;
+    double alpha;
+    double beta;
+    double p_min;
+    double p_max;
+    double p_step;
+};
+
+/* 
+ * EXH: exhaustive method
+ * OUR: our method
+ */
+enum cmd {EXH, OUR};
+
+/* DAG type */
+enum dag_type {A, B, C, D, E, F};
+
+/* function prototypes */
+static int parse_options(int argc, char *argv[], int *cmd, int *dag_type, double e[], struct options *o);
+static int run_ours(int dag_type, double e[], struct options *o);
+static int run_exhaustive(int dag_type, double e[], struct options *o);
+static int get_n(int dag_type);
+static double east(int dag_type, double e[]);
+static double jconv(int dag_type, double p[], double e[]);
 static double util(double p[], double e[], int n);
-static void show_result(const char *method, double j, double u, double p[], int n, long secs, long usecs);
+static void show_result(const char *method, double j, double u, double p[], int n, long secs, long usecs, int timing);
 static double jmax(int n);
 static void timediff(struct timeval *start, struct timeval *end, long *secs, long *usecs);
 static void usage(const char *prog);
@@ -34,99 +59,191 @@ static void usage(const char *prog);
 int main(int argc, char *argv[])
 {
     double e[N_MAX + 1];
-    char *cmd;
-    char t;
-    int i, n, rc;
+    int cmd, dag_type;
+    int i, rc;
+    struct options o;
 
-    if (argc < 3) {
-        fprintf(stderr, "Algorithm and DAG type are mandatory\n");
-        usage(argv[0]);
-        return 1;
-    }
-    cmd = argv[1];
-    if (strlen(argv[2]) != 1) {
-        fprintf(stderr, "Invalid DAG type: %s\n", argv[1]);
-        usage(argv[0]);
-        return 1;
-    }
-    t = *(char*)argv[2];
-    n = get_n(t);
-    if (n < 0) {
-        fprintf(stderr, "Invalid DAG type: %c\n", t);
-        usage(argv[0]);
-        return 1;
-    }
-    if (n + 3 != argc) {
-        fprintf(stderr, "Not enough arguments\n");
-        usage(argv[0]);
-        return 1;
-    }
-    for (i = 1; i <= n; i++) {
-        e[i] = atof(argv[i + 2]);
-    }
+    o.timing = 0;
+    o.ub = UB;
+    o.alpha = ALPHA;
+    o.beta = BETA;
+    o.p_min = P_MIN;
+    o.p_max = P_MAX;
+    o.p_step = P_STEP;
 
-    if (strncmp(cmd, "exh", 3) == 0) {
-        rc = run_exhaustive(e, t, n);
+    rc = parse_options(argc, argv, &cmd, &dag_type, e, &o);
+    if (rc < 0) {
+        return EXIT_FAILURE;
     }
-    else if (strncmp(cmd, "our", 3) == 0) {
-        rc = run_ours(e, t, n);
+    if (cmd == EXH) {
+        rc = run_exhaustive(dag_type, e, &o);
+    }
+    else if (cmd == OUR) {
+        rc = run_ours(dag_type, e, &o);
     }
     else {
-        fprintf(stderr, "Invalid command: %s\n", cmd);
+        fprintf(stderr, "Invalid command number: %d\n", cmd);
     }
-    return rc;
+    if (rc < 0) {
+        return EXIT_FAILURE;
+    }
+    else {
+        return EXIT_SUCCESS;
+    }
+    /* unreachable */
 }
 
-static int run_ours(double e[], char t, const int n)
+static int parse_options(int argc, char *argv[], int *cmd, int *dag_type, double e[], struct options *o)
+{
+    int i, n, opt;
+	while ((opt = getopt(argc, argv, "tm:M:s:a:b:u:")) != -1) {
+        switch (opt) {
+            case 't':
+                o->timing = 1;
+                break;
+            case 'm':
+                o->p_min = atol(optarg);
+                break;
+            case 'M':
+                o->p_max = atol(optarg);
+                break;
+            case 's':
+                o->p_step = atol(optarg);
+                break;
+            case 'a':
+                o->alpha = atol(optarg);
+                break;
+            case 'b':
+                o->beta = atol(optarg);
+                break;
+            case 'u':
+                o->ub = atol(optarg);
+                break;
+            default:
+                usage(argv[0]);
+                return -1;
+        }
+    }
+    if (argc < optind + 2) {
+        fprintf(stderr, "Failed to parse the command line option\n");
+        usage(argv[0]);
+        return -1;
+    }
+    if (strcmp(argv[optind], "exh") == 0 || strcmp(argv[optind], "EXH") == 0) {
+        *cmd = EXH;
+    }
+    else if (strcmp(argv[optind], "our") == 0 || strcmp(argv[optind], "OUR") == 0) {
+        *cmd = OUR;
+    }
+    else {
+        fprintf(stderr, "Invalid command: %s\n", argv[optind]);
+        usage(argv[0]);
+        return -1;
+    }
+    optind++;
+    if (strcmp(argv[optind], "a") == 0 || strcmp(argv[optind], "A") == 0) {
+        *dag_type = A;
+    }
+    else if (strcmp(argv[optind], "b") == 0 || strcmp(argv[optind], "B") == 0) {
+        *dag_type = B;
+    }
+    else if (strcmp(argv[optind], "c") == 0 || strcmp(argv[optind], "C") == 0) {
+        *dag_type = C;
+    }
+    else if (strcmp(argv[optind], "d") == 0 || strcmp(argv[optind], "D") == 0) {
+        *dag_type = D;
+    }
+    else {
+        fprintf(stderr, "Invalid DAG type: %s\n", argv[optind]);
+        usage(argv[0]);
+        return -1;
+
+    }
+    optind++;
+    n = get_n(*dag_type);
+    if (n < 0) {
+        fprintf(stderr, "Cannot identify n from the DAG type %s\n", argv[optind - 1]);
+        usage(argv[0]);
+        return -1;
+    }
+    if (n > argc - optind) {
+        fprintf(stderr, "Requires %d execution times instead of %d\n", n, argc - optind);
+        usage(argv[0]);
+        return -1;
+    }
+    else if (n < argc - optind) {
+        fprintf(stderr, "Requires %d execution times instead of %d\n", n, argc - optind);
+        usage(argv[0]);
+        return -1;
+    }
+    for (i = 1; i <= n; i++) {
+        e[i] = atof(argv[optind++]);
+    }
+    return 0;
+}
+
+static int run_ours(int dag_type, double e[], struct options *o)
 {
     struct timeval start, end;
     long secs, usecs;
+    int i, n;
     double e_ast;
     double p[N_MAX + 1];
     double p_ast;
-    int i;
+
+    double alpha = o->alpha;
+    double beta = o->beta;
 
     gettimeofday(&start, NULL);
-    e_ast = east(e, t, n);
+    n = get_n(dag_type);
+    e_ast = east(dag_type, e);
 
-    p[1] = e[1] + sqrt((n -2) * e[1] * e_ast) + sqrt((ALPHA + BETA) * e[1] * e[n] / BETA); 
+    p[1] = e[1] + sqrt((n -2) * e[1] * e_ast) + sqrt((alpha + beta) * e[1] * e[n] / beta); 
     p_ast = p[1] * sqrt((n - 2) * e_ast / e[1]);
     for (i = 2; i <= n - 1; i++) {
         p[i] = p_ast * (e[i] / e_ast);
     }
-    p[n] = p[1] * sqrt(BETA * e[n] / ((ALPHA + BETA) * e[1]));
+    p[n] = p[1] * sqrt(beta * e[n] / ((alpha + beta) * e[1]));
     gettimeofday(&end, NULL);
     timediff(&start, &end, &secs, &usecs);
 
-    show_result("OURS", jconv(p, e, t, n), util(p, e, n), p, n, secs, usecs);
+    show_result("OURS", jconv(dag_type, p, e), util(p, e, n), p, n, secs, usecs, o->timing);
     
     return 0;
 }
 
-static int run_exhaustive(double e[], char t, const int n)
+static int run_exhaustive(int dag_type, double e[], struct options *o)
 {
     struct timeval start, end;
     long secs, usecs;
-    int i;
+    int i, n;
     double p[N_MAX + 1];
     double opt_p[N_MAX + 1];
     double min_j, min_j_u;
     double u, opt_u;
     double j;
 
+    double ub = o->ub;
+    double alpha = o->alpha;
+    double beta = o->beta;
+    double p_min = o->p_min;
+    double p_max = o->p_max;
+    double p_step = o->p_step;
+
     gettimeofday(&start, NULL);
+    n = get_n(dag_type);
     min_j = jmax(n);
     for (i = 1; i <= n; i++) {
-        p[i] = P_MIN;
+        p[i] = p_min;
     }
     for (;;) {
         int end_flag = 0;
         /* loop body begins here */
         u = util(p, e, n);
-        if (u > UB) {
+        if (u > ub) {
             goto loop;
         }
-        j = jconv(p, e, t, n);
+        j = jconv(dag_type, p, e);
         if (j < min_j) {
             min_j = j;
             min_j_u = u;
@@ -139,9 +256,9 @@ loop:
                 end_flag = 1;
                 break;
             }
-            p[i] += P_STRIDE;
-            if (p[i] > P_MAX) {
-                p[i] = P_MIN;
+            p[i] += p_step;
+            if (p[i] > p_max) {
+                p[i] = p_min;
             }
             else {
                 break;
@@ -153,63 +270,55 @@ loop:
     }
     gettimeofday(&end, NULL);
     timediff(&start, &end, &secs, &usecs);
-    show_result("EXHAUSTIVE", min_j, min_j_u, opt_p, n, secs, usecs); 
+    show_result("EXHAUSTIVE", min_j, min_j_u, opt_p, n, secs, usecs, o->timing); 
 
     return 0;
 }
 
-static int get_n(char t)
+static int get_n(int dag_type)
 {
-    switch (t) {
-    case 'a':
+    switch (dag_type) {
+    case A:
         return 4;
-    case 'b':
+    case B:
         return 5;
-    case 'c':
+    case C:
         return 5;
-    case 'd':
+    case D:
         return 6;
     default:
         return -1;
     }
 }
 
-static double east(double e[], char t, int n)
+static double east(int dag_type, double e[])
 {
-    switch (t) {
-    case 'a':
+    switch (dag_type) {
+    case A:
         return e[2] + e[3];
-    case 'b':
+    case B:
         return MAX2(e[2] + e[3], e[2] + e[4]);
-    case 'c':
+    case C:
         return MAX2(e[4], e[2] + e[3]);
-    case 'd':
+    case D:
         return MAX3(e[2] + e[3], e[2] + e[5], e[4] + e[5]);
-    case 'e':
-        return MAX2(e[2], e[3]);
-    case 'f':
-        return MAX2(e[2] + e[3], e[4]);
-    case 'g':
-        return MAX3(e[2], e[3], e[4]);
-    case 'h':
-        return MAX2(e[2] + e[3], e[4] + e[5]);
     default:
         return 0;
     }
 }
 
-static double jconv(double p[], double e[], char t, int n)
+static double jconv(int dag_type, double p[], double e[])
 {
-    switch (t) {
-    case 'a':
+    switch (dag_type) {
+    case A:
         return (2 * ALPHA * p[4] + 2 * BETA * (p[1] + p[2] + p[3] + p[4]));
-    case 'b':
+    case B:
         return MAX2(2 * ALPHA * p[5] + 2 * BETA * (p[1] + p[2] + p[3] + p[5]),
                     2 * ALPHA * p[5] + 2 * BETA * (p[1] + p[2] + p[4] + p[5]));
-    case 'c':
+    case C:
         return MAX2(2 * ALPHA * p[5] + 2 * BETA * (p[1] + p[4] + p[5]),
                     2 * ALPHA * p[5] + 2 * BETA * (p[1] + + p[2] + p[3] + p[5]));
-    case 'd':
+    case D:
         return MAX3(2 * ALPHA * p[6] + 2 * BETA * (p[1] + p[2] + p[3] + p[6]),
                     2 * ALPHA * p[6] + 2 * BETA * (p[1] + p[2] + p[5] + p[6]),
                     2 * ALPHA * p[6] + 2 * BETA * (p[1] + p[4] + p[5] + p[6]));
@@ -243,7 +352,7 @@ static double util(double p[], double e[], int n)
     /* unreachable */
 }
 
-static void show_result(const char *method, double j, double u, double p[], int n, long secs, long usecs) 
+static void show_result(const char *method, double j, double u, double p[], int n, long secs, long usecs, int timing) 
 {
     int i;
 
@@ -256,11 +365,12 @@ static void show_result(const char *method, double j, double u, double p[], int 
             printf("%-3.2f)", p[i]);
         }
     }
-#if SHOW_TIMING == 1 
-    printf(" <%ld.%06ld>\n", secs, usecs);
-#else
-    printf("\n");
-#endif
+    if (timing) {
+        printf(" <%ld.%06ld>\n", secs, usecs);
+    }
+    else {
+        printf("\n");
+    }
     return;
 }
 
